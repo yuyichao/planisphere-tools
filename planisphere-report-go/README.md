3f92b0d2d53078e9e8f74f827558e8af18900de8
Drew Stinnett <drew.stinnett@duke.edu>
2021-07-31 17:37:46 -0400

    lots of little fixes

--- planisphere-report-go/README.md
+++ planisphere-report-go/README.md
@@ -7,6 +7,10 @@ Do some minimal setup by adding a `~/.planisphere-report.yaml`
 key: foo
 ## Uncomment to use test
 # url: https://planisphere-test.oit.duke.edu/self_report
+
+## Overrides will always take precedence over auto-detected values
+overrides:
+  username: Joe User
 ```
 
 These can also be set with ENV vars using a prefix of `PLANISPHEREREPORT_`, example:
--- planisphere-report-go/helpers/lookup.go
+++ planisphere-report-go/helpers/lookup.go
@@ -1,9 +1,35 @@
 package helpers
 
-type PlanisphereReportLookupOp struct {
-	PlatormLookups *PlatformTool
+import (
+	"net"
+
+	"gitlab.oit.duke.edu/devil-ops/planisphere-sdk/planisphere"
+)
+
+/*
+Lookuper will do the more advanced lookups. Using a custom struct for this so we
+don't have to make duplicate system calls to look at the system_profiler
+*/
+type Lookuper struct {
+	Overrides map[string]interface{}
+	Payload   planisphere.SelfReportPayload
 }
 
-type PlatformTool interface {
-	Memory() (uint64, error)
+/*
+This is pretty OS agnostic, but if we need to later, we can break it out in to
+OS specific functions
+*/
+func getMacAddr() ([]string, error) {
+	ifas, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	var as []string
+	for _, ifa := range ifas {
+		a := ifa.HardwareAddr.String()
+		if a != "" {
+			as = append(as, a)
+		}
+	}
+	return as, nil
 }
